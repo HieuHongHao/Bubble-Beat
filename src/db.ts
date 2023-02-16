a405e87d285c5e69e3a4a5227625d97b9876e84a
Hieu Hong Hao <hieunguyen@vl965-172-31-109-238.wireless.umass.edu>
2023-02-16 10:59:52 -0500

    reconfig db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,25 +1,17 @@
-import { PrismaClient } from "@prisma/client";
-
-
-
-declare global {
-  namespace NodeJS {
-    interface Global {
-      prisma: PrismaClient;
-    }
-  }
-}
-
-
-
+import { PrismaClient } from '@prisma/client';
 
 let prisma: PrismaClient;
-if (!global.prisma) {
-  global.prisma = new PrismaClient({
-    log: ["info"],
-  });
-}
-prisma = global.prisma;
 
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  let globalWithPrisma = global as typeof globalThis & {
+    prisma: PrismaClient;
+  };
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient();
+  }
+  prisma = globalWithPrisma.prisma;
+}
 
 export default prisma;
